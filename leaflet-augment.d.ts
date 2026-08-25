73f416163fea8296af8bd75b22f156cdc9f721d2
teamr0822-art <teamr0822@gmail.com>
2026-08-26 00:23:21 +0900

    Add files via upload

---
+++ leaflet-augment.d.ts
@@ -0,0 +1,18 @@
+import "leaflet";
+
+declare module "leaflet" {
+  interface MapOptions {
+    /** Enable map rotation (provided by leaflet-rotate). */
+    rotate?: boolean;
+    /** Allow a two-finger twist to rotate the map. */
+    touchRotate?: boolean;
+    /** Initial bearing, in degrees clockwise from north. */
+    bearing?: number;
+    /** The plugin's built-in compass control (we ship our own). */
+    rotateControl?: boolean;
+  }
+  interface Map {
+    setBearing(bearing: number): this;
+    getBearing(): number;
+  }
+}
